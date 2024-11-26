336b515295ed1a0b0488fa39d07ec9b6c3e267a9
hasan <mmhotmail>
2024-11-26 18:51:48 +0300

    searcihng

--- src/middlewares/filterPaginate.js
+++ src/middlewares/filterPaginate.js
@@ -14,13 +14,45 @@ module.exports = (req, res, next) => {
   // SKIP:
   const skip = page * limit;
 
+  
+  // SEARCINGH:
+  const { bookName, authorName, publisher, publicationYear, city, district } = req.query;
+
+  let searchQuery = {};
+
+  // Add bookName filter if provided
+  if (bookName) {
+    searchQuery.name = { $regex: bookName, $options: 'i' }; // Partial match, case-insensitive
+  }
+
+  // Add authorName filter if provided
+  if (authorName) {
+    searchQuery.author = { $regex: authorName, $options: 'i' }; // Partial match, case-insensitive
+  }
+
+  // Add publisher filter if provided
+  if (publisher) {
+    searchQuery.bookStore = { $regex: publisher, $options: 'i' }; // Partial match, case-insensitive
+  }
+
+  // Add publicationYear filter if provided
+  if (publicationYear) {
+    searchQuery.publishmentYear = publicationYear; // Exact match for publicationYear
+  }
+
   // Run SearchingSortingPagination engine for Model:
   res.getModelList = async function (Model, filters = {}, populate = null) {
-    const filtersAndSearch = {...filters};
-    const data = await Model.find(filtersAndSearch)
+    const filtersAndSearch = { ...filters, ...searchQuery };
+    let data = await Model.find(filtersAndSearch)
       .skip(skip)
       .limit(limit)
       .populate(populate);
+      if(city){
+        data = data?.filter((book)=>book?.user_id?.city?.value == city)
+      }
+      if(district){
+        data = data?.filter((book)=>book?.user_id?.district?.value == district)
+      }
     const dataCount = await Model.count(filtersAndSearch);
     return { data, dataCount };
   };
